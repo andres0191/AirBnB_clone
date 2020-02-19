743667006299891fbbeaac980562073996c31e5c
jhosep7 <jodia.ms@outlook.com>
2020-02-19 16:04:10 -0500

    tests/test_models/test_save_reload_user.py

--- tests/test_models/test_save_reload_user.py
+++ tests/test_models/test_save_reload_user.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
-from models import storage
+from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
 from models.user import User
+
+storage = FileStorage()
+storage.reload()
 
 all_objs = storage.all()
 print("-- Reloaded objects --")
