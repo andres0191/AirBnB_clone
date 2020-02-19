af3fdc583e4bdca75d68c79f981db0c627ac8470
andres0191 <andres_felipe0191@hotmail.com>
2020-02-19 23:08:34 +0000

    modify test_console.py

--- tests/test_console.py
+++ tests/test_console.py
@@ -7,8 +7,13 @@ from models.place import Place
 from models.amenity import Amenity
 from models.state import State
 from models.review import Review
+
+
 class Testmodels(unittest.TestCase):
+    """Test Models"""
+
     def test_class(self):
+        """Test Class"""
         city1 = City()
         amenity1 = Amenity()
         state1 = State()
@@ -19,7 +24,9 @@ class Testmodels(unittest.TestCase):
         self.assertEqual(state1.__class__.__name__, "State")
         self.assertEqual(rev1.__class__.__name__, "Review")
         self.assertEqual(place1.__class__.__name__, "Place")
+
     def test_father(self):
+        """Test Father"""
         city1 = City()
         amenity1 = Amenity()
         state1 = State()
