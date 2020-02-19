74360a6aecb5261b6167d6e077cac41f00a99fd4
jhosep7 <jodia.ms@outlook.com>
2020-02-19 10:40:43 -0500

    user.py

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -22,12 +22,42 @@ def test_pep8_conformance_user(self):
 class Testuser(unittest.TestCase):
     """test class user"""
 
+    @classmethod
+    def setUpClass(cls):
+        cls.my_user = User()
+        cls.my_user.first_name = 'Holberton'
+        cls.my_user.last_name = 'School'
+        cls.my_user.email = '2020@holbertonschool.com'
+        cls.my_user.password = "peer"
+
     def test_User(self):
         """ Test attributes of Class Use"""
-        my_user = User()
-        my_user.first_name = 'Holberton'
-        my_user.last_name = 'School'
-        my_user.email = '2020@holbertonschool.com'
-        self.assertEqual(my_user.first_name, 'Holberton')
-        self.assertEqual(my_user.last_name, 'School')
-        self.assertEqual(my_user.email, '2020@holbertonschool.com')
+        self.assertEqual(self.my_user.first_name, 'Holberton')
+        self.assertEqual(self.my_user.last_name, 'School')
+        self.assertEqual(self.my_user.email, '2020@holbertonschool.com')
+        self.assertEqual(self.my_user.password, 'peer')
+
+    def test_strings(self):
+        """ Test if it's string"""
+        self.assertEqual(type(self.my_user.email), str)
+        self.assertEqual(type(self.my_user.password), str)
+        self.assertEqual(type(self.my_user.first_name), str)
+        self.assertEqual(type(self.my_user.first_name), str)
+
+    def test_save(self):
+        """ Test if saves"""
+        self.my_user.save()
+        self.assertNotEqual(self.my_user.created_at, self.my_user.updated_at)
+
+    def test_to_dict(self):
+        """ Test if in dict"""
+        self.assertEqual('to_dict' in dir(self.my_user), True)
+
+    def test_attr(self):
+        self.assertTrue('email' in self.my_user.__dict__)
+        self.assertTrue('id' in self.my_user.__dict__)
+        self.assertTrue('created_at' in self.my_user.__dict__)
+        self.assertTrue('updated_at' in self.my_user.__dict__)
+        self.assertTrue('password' in self.my_user.__dict__)
+        self.assertTrue('first_name' in self.my_user.__dict__)
+        self.assertTrue('last_name' in self.my_user.__dict__)
